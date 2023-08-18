5c9ab16c547c5f349bf6778a8a90f2dc53b304d2
rv2673 <rv2673@users.noreply.github.com>
2023-08-18 18:45:26 +0200

    Setup minimal cdk app for issue

---
+++ README.md
@@ -0,0 +1,13 @@
+# aws-cdk-cross-region-secret-import-reproduction
+
+This repository contain minimal setup to reproduce for
+aws-cdk issue with cross-region policy reference of imported secret
+
+## Useful commands
+
+* `npm run build`   compile typescript to js
+* `npm run watch`   watch for changes and compile
+* `npm run test`    perform the jest unit tests
+* `npm run cdk deploy`      deploy this stack to your default AWS account/region
+* `npm run cdk diff`        compare deployed stack with current state
+* `npm run cdk synth`       emits the synthesized CloudFormation template
---
+++ bin/secrets-manager-reproduction.ts
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+
+const SECRET_COMPLETE_ARN = 'arn:aws:secretsmanager:eu-central-1:123456789:secret:secret-name-suffix-abcfgh'
+
+const app = new cdk.App();
+
+const containingStack = new cdk.Stack(app, 'containing', {env: {
+  region: 'eu-central-1'
+}})
+const secret = cdk.aws_secretsmanager.Secret.fromSecretCompleteArn(
+  containingStack, 'secret', SECRET_COMPLETE_ARN
+)
+
+const consumingStack = new cdk.Stack(app, 'consuming', {env: {
+  region: 'us-east-1'
+}})
+
+const lambdaRole = new cdk.aws_iam.Role(consumingStack, 'Role', {
+  assumedBy: new cdk.aws_iam.ServicePrincipal('lambda.amazonaws.com'),
+});
+
+secret.grantRead(lambdaRole)
